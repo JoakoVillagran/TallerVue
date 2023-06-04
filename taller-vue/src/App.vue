448c07e15decc69d0147e99cddaf272c1e576396
JoakoVillagran <89611124+JoakoVillagran@users.noreply.github.com>
2023-06-03 21:28:27 -0400

    Revert "Vista2 merge completo"

--- taller-vue/src/App.vue
+++ taller-vue/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <router-view></router-view>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
--- taller-vue/src/services/api-services.js
+++ taller-vue/src/services/api-services.js
@@ -1,10 +1,3 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://ec2-54-163-208-73.compute-1.amazonaws.com:8080';
-
-function getDetalles (id) {
-    return axios.get(`${BASE_URL}/products/${id}/reviews`).then((result) => {
-      return result.data;
-    });
-}
-export {getDetalles};
