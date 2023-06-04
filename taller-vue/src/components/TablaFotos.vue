abe935e69f99fffff00917516ed6d582d64364fd
JoakoVillagran <j.villagran04@ufromail.cl>
2023-06-03 21:57:32 -0400

    tablafotos final

--- taller-vue/src/components/TablaFotos.vue
+++ taller-vue/src/components/TablaFotos.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <table class="table">
       <tbody>
@@ -29,34 +28,3 @@ export default {
   }
 }
 </script>
-=======
-    <div>
-      <table class="table">
-        <tbody>
-          <tr v-for="resena in resenas" :key="resena.id">
-            <td v-if="perfil"><img :src="resena.product.images[0]" width="100" height="100" /></td>
-            <td v-else><img :src="resena.user.photo" width="100" height="100" /></td>
-            <td v-if="perfil">{{ resena.product.name }}</td>
-            <td v-else>{{ resena.user.name }}</td>
-            <td>{{ resena.review }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  
-  
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      resenas: {
-        type: Array,
-      },
-      perfil: {
-        type: Boolean
-      }
-    }
-  }
-  </script>
->>>>>>> vista2
